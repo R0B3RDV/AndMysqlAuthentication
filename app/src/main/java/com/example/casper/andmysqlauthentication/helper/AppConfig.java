807f1ac95e3a5cb64ec9629f38e5862b1699b3ff
Samet <sametay153@gmail.com>
2018-05-01 15:34:51 +0300

    app config login / register urls - Volley app controller - Manifest internet permission

---
+++ app/src/main/java/com/example/casper/andmysqlauthentication/helper/AppConfig.java
@@ -0,0 +1,8 @@
+package com.example.casper.andmysqlauthentication.helper;
+
+public class AppConfig {
+    // Server user giriş url
+    public static String URL_LOGIN = "http://192.168.2.36/Andauth/login.php";
+    // Server user kayıt url
+    public static String URL_REGISTER = "http://192.168.2.36/Andauth/register.php";
+}
