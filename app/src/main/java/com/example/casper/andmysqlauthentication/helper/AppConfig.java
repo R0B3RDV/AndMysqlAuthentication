6940a2484a397367e8326fb2e8ad6ae57096d88e
Samet <sametay153@gmail.com>
2018-05-17 04:23:05 +0300

    Design update

--- app/src/main/java/com/example/casper/andmysqlauthentication/helper/AppConfig.java
+++ app/src/main/java/com/example/casper/andmysqlauthentication/helper/AppConfig.java
@@ -2,7 +2,7 @@ package com.example.casper.andmysqlauthentication.helper;
 
 public class AppConfig {
     // Server user giriş url
-    public static String URL_LOGIN = "http://192.168.2.36/Andauth/login.php";
+    public static String URL_LOGIN = "http://10.9.3.227/Andauth/login.php";
     // Server user kayıt url
-    public static String URL_REGISTER = "http://192.168.2.36/Andauth/register.php";
+    public static String URL_REGISTER = "http://10.9.3.227/Andauth/register.php";
 }
