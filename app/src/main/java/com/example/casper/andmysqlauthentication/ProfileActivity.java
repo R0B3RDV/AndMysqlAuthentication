50fa3dc088534bbb3015e6230d5e390b28f5f02f
Samet <sametay153@gmail.com>
2018-05-17 11:14:41 +0300

    READ_EXTERNAL_STORAGE permission/profil photo click gallery image select

--- app/src/main/java/com/example/casper/andmysqlauthentication/ProfileActivity.java
+++ app/src/main/java/com/example/casper/andmysqlauthentication/ProfileActivity.java
@@ -1,5 +1,7 @@
 package com.example.casper.andmysqlauthentication;
 
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.casper.andmysqlauthentication.helper.SessionManager;
@@ -9,6 +11,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
     private TextView txtName;
@@ -18,7 +21,8 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView txtCreated;
     private TextView txtEmail;
     private Button btnLogout;
-
+    private de.hdodenhof.circleimageview.CircleImageView pp;
+    private static final int RESULT_LOAD_IMAGE=1;
     private SQLiteHandler db;
     private SessionManager session;
 
@@ -33,7 +37,7 @@ public class ProfileActivity extends AppCompatActivity {
         txtCreated = (TextView) findViewById(R.id.created);
         txtEmail = (TextView) findViewById(R.id.email);
         btnLogout = (Button) findViewById(R.id.btnLogout);
-
+        pp = (de.hdodenhof.circleimageview.CircleImageView) findViewById(R.id.pp);
         // SqLite database handler
         db = new SQLiteHandler(getApplicationContext());
 
@@ -70,6 +74,23 @@ public class ProfileActivity extends AppCompatActivity {
                 logoutUser();
             }
         });
+
+        pp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data !=null){
+            Uri selectedImage = data.getData();
+            pp.setImageURI(selectedImage);
+        }
     }
 
     private void logoutUser() {
